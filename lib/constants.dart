8ae33fbe53f53ae6886b2563cd704d726dfb0708
Aldairtunq <201912945b@utea.edu.pe>
2022-12-21 17:06:50 -0500

    creamos listado de colores que vamos a utilizar

---
+++ lib/constants.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+// listado delos colores
+
+const KBackgroundColor = Color(0xFFF1EFF1);
+
+const KPrimaryColor = Color(0xFF035AA6);
+
+const KSecondaryColor = Color(0xFFFFA41B);
+
+const KTextColor = Color(0xFF000839);
+
+const KTextLightColor = Color(0xFF747474);
+
+const KBlueColor = Color(0xFF40BAD5);
+
+const KDefaultPadding = 20.0;
+
+//sombra por defecto
+
+const KDefaultShadow = BoxShadow(
+    offset: Offset(0, 15),
+    blurRadius: 27,
+    color: Colors.black12 //black color with 12%  opacity
+    );
